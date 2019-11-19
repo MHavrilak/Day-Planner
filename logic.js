c5e1798340344edc032f38cc7a46172aaaab8c0c
Hava-hizzle <mikehavrilak@gmail.com>
2019-11-18 20:39:38 -0500

    revisions

---
+++ logic.js
@@ -0,0 +1,40 @@
+var min = "nine";
+var max = "seventeen";
+var currHr = "";
+
+var currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do, h:mm A"));
+
+
+
+moment().format('MMMM Do YYYY, h:mm:ss a');
+
+var hours = moment().hours();
+
+function dayPlanner() {
+    if( currHr > min ) {
+        "time-block row".setAttribute(.past);
+    } else {
+        "time-block row".setAttribute(.present)
+    } else if {
+        "time-block row".setAttribute(.future)
+    };
+
+}
+var textInput = "";
+
+    localStorage.setItem("nine", "");
+    var taskAt9am = localStorage.getItem("nine");
+
+    localStorage.setItem("nine", JSON.stringify(textInput));
+    var taskAt9am = JSON.parse(localStorage.getItem("nine"));
+
+    
+
+
+
+
+
+
+
+
+    
